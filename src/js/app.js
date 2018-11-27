b1df58eb0827f8c2e2791952d676aeb89ee8b89d
rajashekarreddybobbili <b.rajareddy1903@gmail.com>
2018-11-27 08:11:42 +0530

    latest develop

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,67 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+$(document).on('ready', function() {
+  $('.center').slick({
+  	  dots:true,
+	  centerMode: true,
+	  focusOnSelect: true,
+	  centerPadding: '100px',
+	  slidesToShow: 1,
+	  responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '0px',
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '0px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+  $('a[data-slide]').click(function(e) {
+   e.preventDefault();
+   $(this).addClass("active-slider");
+   var slideno = $(this).data('slide');
+   $('.center').slick('slickGoTo', slideno - 1);
+ });
+
+  $('.centerMobile').slick({
+  	  dots:true,
+	  centerMode: true,
+	  focusOnSelect: true,
+	  centerPadding: '50px',
+	  slidesToShow: 1,
+	  mobileFirst: true,
+	  responsive: [
+	    {
+	       breakpoint: 600,
+	       settings: 'unslick'
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: false,
+	        centerMode: true,
+	        centerPadding: '0px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+});
+
+// $("a.nav-link").click(function(){
+// 	console.log(this);
+// });
