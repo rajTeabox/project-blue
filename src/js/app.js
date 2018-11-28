3a78c8e08e72735741b09a83a2e9984fb86fd389
rajashekarreddybobbili <b.rajareddy1903@gmail.com>
2018-11-28 10:35:57 +0530

    development

--- src/js/app.js
+++ src/js/app.js
@@ -34,8 +34,42 @@ $(document).on('ready', function() {
 	    }
 	  ]
 	});
+  // testimonial slider
+  $('.testimonials').slick({
+  	  dots:true,
+  	  arrows: false,
+	  centerMode: true,
+	  focusOnSelect: true,
+	  centerPadding: '0px',
+	  slidesToShow: 1,
+	  adaptiveHeight: true,
+	  responsive: [
+	    {
+	      breakpoint: 768,
+	      settings: {
+	      	arrows: false,
+	      	adaptiveHeight: true,
+	        centerMode: true,
+	        centerPadding: '0px',
+	        slidesToShow: 1
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        arrows: false,
+	        adaptiveHeight: true,
+	        centerMode: true,
+	        centerPadding: '0px',
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+
   $('a[data-slide]').click(function(e) {
    e.preventDefault();
+   $('a[data-slide]').removeClass("active-slider");
    $(this).addClass("active-slider");
    var slideno = $(this).data('slide');
    $('.center').slick('slickGoTo', slideno - 1);
@@ -66,6 +100,16 @@ $(document).on('ready', function() {
 	});
 });
 
-// $("a.nav-link").click(function(){
-// 	console.log(this);
-// });
+$(".feature_header").click(function(){
+	closeNav();
+  $('html, body').animate({
+   scrollTop: $(".featured-work-space").offset().top
+ }, 500);
+});
+
+$(".service_header").click(function(){
+	closeNav();
+  $('html, body').animate({
+   scrollTop: $(".our-service").offset().top
+ }, 500);
+});
